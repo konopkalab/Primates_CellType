b4a070ed7c003156000b2330a92a2c4859b094c2
Stefano Berto <stefano.berto@utsouthwestern.edu>
2019-10-24 18:03:58 -0500

    Update Viz_Combined_Primates.R

--- DGE/Viz_Combined_Primates.R
+++ DGE/Viz_Combined_Primates.R
@@ -21,6 +21,8 @@ suppressPackageStartupMessages(library(tibble))
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(here))
 suppressPackageStartupMessages(library(ggforce))
+suppressPackageStartupMessages(library(ape))
+suppressPackageStartupMessages(library(graphics))
 
 # Boxplot difference estimate size
 n <- read.table(here("OUTPUTS_EXONS_NEUN","NEUN_PRIMATES_DGE_HUMAN_SPECIFIC.txt"),header=T,sep="\t")
@@ -34,29 +36,27 @@ df <- rbind(n,o)
 df$absHC <- abs(df$Estimate_HsapVsPanTro)
 df$absHR <- abs(df$Estimate_HsapVsRheMac)
 
-A <- ggviolin(df, 
+A <- ggboxplot(df, 
       		x = "Class", 
       		y = "Estimate_HsapVsPanTro",
-          	fill = "Class", 
-          	add = "boxplot", add.params = list(fill = "white"),
+          	color = "Class", 
           	palette = c("cyan4","magenta4"),
           	short.panel.labs = FALSE) + 
           	stat_compare_means(label = "p.format",method = "t.test",label.x = 1.5)+
           	theme_classic()+
-        		labs(title="Human vs Chimpanzee", subtitle= "Human Specific DGE",x="", y = "log2(Fold Change)")+
+        		labs(title="H vs C: DGE",x="", y = "log2(Fold Change)")+
         		theme(legend.position="none")+
           	rotate_x_text(angle = 45)
 
-C <- ggviolin(df, 
+C <- ggboxplot(df, 
       		x = "Class", 
       		y = "Estimate_HsapVsRheMac",
-          	fill = "Class", 
-          	add = "boxplot", add.params = list(fill = "white"), 
+          	color = "Class", 
           	palette = c("cyan4","magenta4"),
           	short.panel.labs = FALSE) + 
           	stat_compare_means(label = "p.format",method = "t.test",label.x = 1.5)+
           	theme_classic()+
-        		labs(title="Human vs Macaque",subtitle= "Human Specific DGE",x="", y = "log2(Fold Change)")+
+        		labs(title="H vs M: DGE",x="", y = "log2(Fold Change)")+
         		theme(legend.position="none")+
           	rotate_x_text(angle = 45)
 
@@ -73,35 +73,33 @@ df2 <- rbind(n,o)
 df2$absHC <- abs(df2$Estimate_HsapVsPanTro)
 df2$absHR <- abs(df2$Estimate_HsapVsRheMac)
 
-B <- ggviolin(df2, 
+B <- ggboxplot(df2, 
       		x = "Class", 
       		y = "Estimate_HsapVsPanTro",
-          	fill = "Class", 
-          	add = "boxplot", add.params = list(fill = "white"), 
+          	color = "Class", 
           	palette = c("cyan4","magenta4"),
           	short.panel.labs = FALSE) + 
           	stat_compare_means(label = "p.format",method = "t.test",label.x = 1.5)+
           	theme_classic()+
-        		labs(title="Human vs Chimpanzee", subtitle= "All Genes",x="", y = "log2(Fold Change)")+
+        		labs(title="H vs C: All Genes",x="", y = "log2(Fold Change)")+
         		theme(legend.position="none")+
           	rotate_x_text(angle = 45)
 
-D <- ggviolin(df2, 
+D <- ggboxplot(df2, 
       		x = "Class", 
       		y = "Estimate_HsapVsRheMac",
-          	fill = "Class", 
-          	add = "boxplot", add.params = list(fill = "white"),
+          	color = "Class", 
           	palette = c("cyan4","magenta4"),
           	short.panel.labs = FALSE) + 
           	stat_compare_means(label = "p.format",method = "t.test",label.x = 1.5)+
           	theme_classic()+
-        		labs(title="Human vs Macaque",subtitle= "All Genes",x="", y = "log2(Fold Change)")+
+        		labs(title="H vs M: All Genes",x="", y = "log2(Fold Change)")+
         		theme(legend.position="none")+
           	rotate_x_text(angle = 45)
 
 
 plot2by2 <- cowplot::plot_grid(A,B,C,D,labels=c("A", "B","C","D"), ncol = 2)
-cowplot::save_plot("Boxplots_Estimates.pdf", plot2by2, ncol = 2,base_height=6,base_width=3)
+cowplot::save_plot("Boxplots_Estimates.pdf", plot2by2, ncol = 2,base_height=5,base_width=2.5)
 
 # Barplot number of DGE
 data <- data.frame(Species = c("Hsap","PanTro","RheMac","Hsap","PanTro","RheMac"),CellType = c(rep("NeuN",3),rep("OLIG2",3)), Up=c(313,141,437,354,99,337),Down=c(181,134,457,179,64,293))
@@ -120,5 +118,97 @@ ggbarplot(df, "Species", "value",
   xlab = "", ylab = "", facet.by = "CellType")+
 theme_classic()+
 ylim(0,700)
+dev.off()
+
+# unrooted tree
+library(ape)
+n <- read.tree(text="(RheMac:19.9,(PanTro:45.8,Hsap:82.4):1);")
+
+pdf("ChangesXyear_Tree_NeuN_New.pdf",width=5,height=5,useDingbats=FALSE)
+plot(as.phylo(n), type = "unrooted", cex = 0.6,
+     edge.color = "cyan4", edge.width = 2, edge.lty = 2,
+     tip.color = "cyan4")
+edgelabels(n$edge.length, bg = "white",col="black", font=2)
+dev.off()
+
+o <- read.tree(text="(RheMac:14,(PanTro:27.2,Hsap:88.8):1);")
+
+pdf("ChangesXyear_Tree_OLIG2_New.pdf",width=5,height=5,useDingbats=FALSE)
+plot(as.phylo(o), type = "unrooted", cex = 0.6,
+     edge.color = "magenta4", edge.width = 2, edge.lty = 2,
+     tip.color = "magenta4")
+edgelabels(o$edge.length, bg = "white",col="black", font=2)
+dev.off()
+
+# Matrix 
+mat <- as.data.frame(matrix(c(82.4,45.8, 19.9,88.8,27.2,14),nc=2))
+rownames(mat) <- c("H","P","M")
+colnames(mat) <- c("NeuN","OLIG2")
+chisq.test(mat)
+
+# Density of gene expression
+load("DGE_DATA_INPUTS/DGE_Primates_NeuN.RData")
+load("OUTPUTS_EXONS_NEUN/WGCNA_EXONS_NEUN.RData")
+
+dge <- AllStat
+adj <- expAdj
+
+adj$Gene <- rownames(adj)
+NEUN <- melt(adj)
+NEUN$Species <- do.call(rbind,strsplit(as.character(NEUN$variable),"_"))[,2]
+NEUN$Class <- rep("NeuN",nrow(NEUN))
+
+load("DGE_DATA_INPUTS/DGE_Primates_OLIG2.RData")
+load("OUTPUTS_EXONS_OLIG2/WGCNA_EXONS_OLIG2.RData")
+
+dge <- AllStat
+adj <- expAdj
+
+adj$Gene <- rownames(adj)
+OLIG2 <- melt(adj)
+OLIG2$Species <- do.call(rbind,strsplit(as.character(OLIG2$variable),"_"))[,2]
+OLIG2$Class <- rep("OLIG2",nrow(OLIG2))
+
+df <- rbind(NEUN,OLIG2)
+
+pdf("Density_Expression_Values.pdf",width=4,height=2,useDingbats=FALSE)
+ggdensity(df, x = "value",
+   add = "mean", rug = FALSE,
+   color = "Class", palette = c("cyan4", "magenta4"),
+   facet.by="Species",
+   alpha=0.5) +
+theme_classic()+
+xlab("Adjusted Expression")+
+theme(legend.position="none")
+dev.off()
+
+# Mosaic Plot and Fisher exact
+M <- as.table(rbind(c(494,533), c(7878,7027)))
+dimnames(M) <- list(Class = c("DEG", "NotDEG"),
+                    Cell = c("NeuN","OLIG2"))
+
+fisher.test(M,alternative="l")
+
+pdf("MosaicPlot_Hsap.pdf",width=4,height=4,useDingbats=FALSE)
+mosaicplot(M, shade = TRUE, las=2,main = "Hsap")
+dev.off()
+
+
+M <- as.table(rbind(c(275,163), c(8097,7397)))
+dimnames(M) <- list(Class = c("DEG", "NotDEG"),
+                    Cell = c("NeuN","OLIG2"))
+
+fisher.test(M,alternative="l")
+
+pdf("MosaicPlot_PanTro.pdf",width=4,height=4,useDingbats=FALSE)
+mosaicplot(M, shade = TRUE, las=2,main = "PanTro")
+dev.off()
+
+M <- as.table(rbind(c(894,630), c(7478,6930)))
+dimnames(M) <- list(Class = c("DEG", "NotDEG"),
+                    Cell = c("NeuN","OLIG2"))
+
+pdf("MosaicPlot_MacMul.pdf",width=4,height=4,useDingbats=FALSE)
+mosaicplot(M, shade = TRUE, las=2,main = "PanTro")
 dev.off()
 
